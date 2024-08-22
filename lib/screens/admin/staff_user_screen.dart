6b553b536b957dd873d3eebd1987f4eecea8bad9
Praveen <69238094+Praveen919@users.noreply.github.com>
2024-08-22 21:42:01 +0530

    Update staff_user_screen.dart

--- lib/screens/admin/staff_user_screen.dart
+++ lib/screens/admin/staff_user_screen.dart
@@ -1,41 +1,280 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
+class StaffUserScreen extends StatelessWidget {
 
-class Staff_USer_Screen extends StatelessWidget {
-  const Staff_USer_Screen({super.key});
+  final String option;
+
+  StaffUserScreen({this.option = 'createStaff'});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Staff/User'),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text('Create Staff'),
-            onTap: () {
-              // Navigate to Create Staff Screen
-            },
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    switch (option) {
+      case 'createStaff':
+        return CreateStaffScreen();
+      case 'manageStaff':
+        return ManageStaffScreen();
+      case 'manageStaffRights':
+        return ManageStaffRightsScreen();
+      case 'staffAttendance':
+        return StaffAttendanceScreen();
+      default:
+        return Center(child: Text('Unknown Option'));
+    }
+  }
+}
+
+class CreateStaffScreen extends StatefulWidget {
+  @override
+  _CreateStaffScreenState createState() => _CreateStaffScreenState();
+}
+
+class _CreateStaffScreenState extends State<CreateStaffScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _firstNameController = TextEditingController();
+  final _middleNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _genderController = TextEditingController();
+  final _mobileController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _addressController = TextEditingController();
+  XFile? _profilePicture;
+  bool _isEditable = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Staff Setup'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              Text(
+                'Create New Staff',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              _buildTextField('First Name*', _firstNameController),
+              _buildTextField('Middle Name*', _middleNameController),
+              _buildTextField('Last Name*', _lastNameController),
+              _buildTextField('Gender*', _genderController, readOnly: true),
+              _buildTextField('Mobile No*', _mobileController),
+              _buildTextField('Email', _emailController),
+              _buildTextField('Address', _addressController),
+              SizedBox(height: 20),
+              _buildProfilePicturePicker(),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: _resetForm,
+                    child: Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _saveForm,
+                    child: Text('Save'),
+                  ),
+                ],
+              ),
+            ],
           ),
-          ListTile(
-            title: Text('Manage Staff'),
-            onTap: () {
-              // Navigate to Manage Staff screen
-            },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller,
+      {bool readOnly = false}) {
+    return TextFormField(
+      controller: controller,
+      readOnly: !_isEditable || readOnly,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return '$label is required';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildProfilePicturePicker() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Profile Picture:'),
+        SizedBox(height: 10),
+        GestureDetector(
+          onTap: _isEditable ? _pickImage : null,
+          child: Container(
+            height: 150,
+            width: 150,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+            ),
+            child: _profilePicture == null
+                ? Center(child: Text('No image selected'))
+                : Image.file(File(_profilePicture!.path), fit: BoxFit.cover),
           ),
-          ListTile(
-            title: Text('Manage Staff Rights'),
-            onTap: () {
-              // Navigate to Manage Staff Rights screen
-            },
+        ),
+      ],
+    );
+  }
+
+  void _pickImage() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image =
+    await _picker.pickImage(source: ImageSource.gallery);
+
+    if (image != null) {
+      setState(() {
+        _profilePicture = image;
+      });
+    }
+  }
+
+  void _resetForm() {
+    setState(() {
+      _isEditable = true;
+      _firstNameController.clear();
+      _middleNameController.clear();
+      _lastNameController.clear();
+      _genderController.clear();
+      _mobileController.clear();
+      _emailController.clear();
+      _addressController.clear();
+      _profilePicture = null;
+    });
+  }
+
+  void _saveForm() {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isEditable = false;
+      });
+      // Save form data
+      // Perform save operation here, like sending data to a server or saving it locally.
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Staff details saved successfully'),
+      ));
+    }
+  }
+}
+
+class ManageStaffScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Manage Staff Screen'));
+  }
+}
+
+class ManageStaffRightsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Manage Staff Rights Screen'));
+  }
+}
+
+class StaffAttendanceScreen extends StatefulWidget {
+  @override
+  _ManageStaffAttendanceScreenState createState() => _ManageStaffAttendanceScreenState();
+}
+
+class _ManageStaffAttendanceScreenState extends State<StaffAttendanceScreen> {
+  DateTime? selectedDate;
+
+  _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate ?? DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Take Staff Attendance',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Attendance Date: ',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  InkWell(
+                    onTap: () => _selectDate(context),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        selectedDate != null
+                            ? DateFormat('dd/MM/yyyy').format(selectedDate!)
+                            : 'Select Date',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  // Implement your 'Take Attendance' functionality here
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 30),
+                ),
+                child: Text(
+                  'Take Attendance',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            ],
           ),
-          ListTile(
-            title: Text('Staff Attendance'),
-            onTap: () {
-              // Navigate to Staff Attendance screen
-            },
-          ),
-          
-        ],
+        ),
       ),
     );
   }
