a480a18199a7c4725f4c9a5ed8e8b4a2da3a414a
Praveen <69238094+Praveen919@users.noreply.github.com>
2024-09-07 20:36:46 +0530

    Update testConnection.js

--- backend/testConnection.js
+++ backend/testConnection.js
@@ -18,7 +18,7 @@ testConnection();
 */
 const mongoose = require('mongoose');
 
-const mondbUrl="mongodb+srv://praveennaidu297:$APLSP2utmKd$7.@cluster0.qddnp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mondbUrl="mongodb+srv://praveennaidu297:db-pass@cluster0.qddnp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const connectDb=()=>{
     return mongoose.connect(mondbUrl)
