b692da5fb2d2c43394b04a58bd205277bfdcb031
Praveen <69238094+Praveen919@users.noreply.github.com>
2024-09-11 17:37:19 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:testing_app/screens/students/dashboard_screen.dart';
+import 'package:testing_app/screens/teachers/dashboardt.dart';
 import 'screens/loginscreen.dart';
 import 'screens/create_account_screen.dart';
 import 'screens/branch_year_selection_screen.dart';
@@ -22,8 +24,10 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const LoginScreen(),
         '/createAccount': (context) => const CreateAccountScreen(),
-        '/branchYearSelection': (context) => const BranchYearSelectionScreen(),
+        '/branchYearSelection': (context) => const BranchYearSelectionScreen(userRole: ''),
         '/adminDashboard': (context) => const DashboardScreen(),
+        '/teacherDashboard': (context) => const DashboardT(),
+        '/studentDashboard': (context) => const Dashboard1Screen()
       },
     );
   }
