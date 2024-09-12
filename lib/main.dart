ba923dc16b45f81ffed5bf63cc257b95c8d62546
Praveen <69238094+Praveen919@users.noreply.github.com>
2024-09-12 22:05:30 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:testing_app/screens/loginscreen.dart';
 import 'package:testing_app/screens/students/dashboard_screen.dart';
 import 'package:testing_app/screens/teachers/dashboardt.dart';
-import 'screens/loginscreen.dart';
-import 'screens/create_account_screen.dart';
-import 'screens/branch_year_selection_screen.dart';
-import 'screens/admin/dashboard_screen.dart';
+import 'package:testing_app/screens/create_account_screen.dart';
+import 'package:testing_app/screens/branch_year_selection_screen.dart';
+import 'package:testing_app/screens/admin/dashboard_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,13 +21,43 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       initialRoute: '/', // Default route
-      routes: {
+      routes: <String, WidgetBuilder>{
         '/': (context) => const LoginScreen(),
         '/createAccount': (context) => const CreateAccountScreen(),
-        '/branchYearSelection': (context) => const BranchYearSelectionScreen(userRole: ''),
-        '/adminDashboard': (context) => const DashboardScreen(),
-        '/teacherDashboard': (context) => const DashboardT(),
-        '/studentDashboard': (context) => const Dashboard1Screen()
+        // Adjusted routes to handle parameters via arguments
+        '/branchYearSelection': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments as Map<String, String>?;
+          return BranchYearSelectionScreen(
+            userRole: arguments?['userRole'] ?? '',
+            name: arguments?['name'] ?? '',
+            branch: arguments?['branch'] ?? '',
+            year: arguments?['year'] ?? '',
+          );
+        },
+        '/adminDashboard': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments as Map<String, String>?;
+          return DashboardScreen(
+            name: arguments?['name'] ?? '',
+            branch: arguments?['branch'] ?? '',
+            year: arguments?['year'] ?? '',
+          );
+        },
+        '/teacherDashboard': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments as Map<String, String>?;
+          return DashboardT(
+            name: arguments?['name'] ?? '',
+            branch: arguments?['branch'] ?? '',
+            year: arguments?['year'] ?? '',
+          );
+        },
+        '/studentDashboard': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments as Map<String, String>?;
+          return Dashboard1Screen(
+            name: arguments?['name'] ?? '',
+            branch: arguments?['branch'] ?? '',
+            year: arguments?['year'] ?? '',
+          );
+        },
       },
     );
   }
