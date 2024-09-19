6a4bc3a93b057dfcf35bb6533c951eb2437d8bdf
Praveen <69238094+Praveen919@users.noreply.github.com>
2024-09-19 20:11:38 +0530

    Add files via upload

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,18 +1,24 @@
-// models/userModel.js
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    instituteName: { type: String, required: true },
-    country: { type: String, required: true },
-    city: { type: String, required: true },
+    instituteName: { type: String },
+    country: { type: String },
+    city: { type: String },
     name: { type: String, required: true },
     mobile: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true},
     password: { type: String, required: true },
     role: { type: String, enum: ['Admin', 'Teacher', 'Student'], required: true },
-    branch: { type: String },  // New field for branch
-    year: { type: String }     // New field for year
+    branch: { type: String },
+    year: { type: String },
+    feeRecHeader: { type: String },
+    branchAddress: { type: String },
+    taxNo: { type: String },
+    feeFooter: { type: String },
+    logoDisplay: { type: String },
+    feeStatusDisplay: { type: String },
+    chatOption: { type: String },
+    profileLogo: { type: String }, // File reference from GridFS
 });
 
 module.exports = mongoose.model('User', userSchema);
